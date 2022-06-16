175c0c5d721423f0f92853de9a20fadaed07ab6a
haywhiz <54290706+haywhiz@users.noreply.github.com>
2022-06-16 01:42:40 +0100

    handling error

--- handlingstrings.c
+++ handlingstrings.c
@@ -1,65 +1,76 @@
 #include "shell.h"
-/**
- * _strcat - concatenate two strings.
- * @src: First string to append.
- * @dest: Second string to append.
- * Return: Pointer to the resulting string.
- **/
-void _strcat(char *dest, char *src)
-{
-	int i = 0, j;
 
-	while (dest[i])
-		i++;
-	for (j = 0; src[j]; j++, i++)
-		dest[i] = src[j];
-	dest[i] = '\0';
-}
 /**
- * _strlen - length of a string.
- * @s: pointer.
- * Return: result.
- */
-int _strlen(char *s)
-{
-	int cifras = 0;
-
-	while (*(s + cifras) != '\0')
-		cifras++;
-	return (cifras);
-}
-/**
- * _strcpy - copies the string pointed to by src.
+ * print_error - prints error messages to standard error
+ * @vars: pointer to struct of variables
+ * @msg: message to print out
  *
- * @dest: pointer.
- * @src: pointer.
- *
- * Return: void.
+ * Return: void
  */
-char *_strcpy(char *dest, char *src)
+void print_error(vars_t *vars, char *msg)
 {
-	int i;
+	char *count;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- * _strcmp - compare two strings.
- * @s1: string #1.
- * @s2: string #2.
- * Return: 0 if s1 and s2 are equal.
- **/
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	_puts2(vars->argv[0]);
+	_puts2(": ");
+	count = _uitoa(vars->count);
+	_puts2(count);
+	free(count);
+	_puts2(": ");
+	_puts2(vars->av[0]);
+	if (msg)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		_puts2(msg);
 	}
-	return (0);
+	else
+		perror("");
+}
+
+/**
+ * _puts2 - prints a string to standard error
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts2(char *str)
+{
+	ssize_t num, len;
+
+	num = _strlen(str);
+	len = write(STDERR_FILENO, str, num);
+	if (len != num)
+	{
+		perror("Fatal Error");
+		exit(1);
+	}
+
+}
+
+/**
+ * _uitoa - converts an unsigned int to a string
+ * @count: unsigned int to convert
+ *
+ * Return: pointer to the converted string
+ */
+char *_uitoa(unsigned int count)
+{
+	char *numstr;
+	unsigned int tmp, digits;
+
+	tmp = count;
+	for (digits = 0; tmp != 0; digits++)
+		tmp /= 10;
+	numstr = malloc(sizeof(char) * (digits + 1));
+	if (numstr == NULL)
+	{
+		perror("Fatal Error1");
+		exit(127);
+	}
+	numstr[digits] = '\0';
+	for (--digits; count; --digits)
+	{
+		numstr[digits] = (count % 10) + '0';
+		count /= 10;
+	}
+	return (numstr);
 }
