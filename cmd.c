bef471591bca9ec3c52b58e51fae93f3b23555e5
haywhiz <54290706+haywhiz@users.noreply.github.com>
2022-06-16 01:32:36 +0100

    builtin continuation

--- cmd.c
+++ cmd.c
@@ -1,52 +1,118 @@
 #include "shell.h"
-/**
- * ex - close the shell process.
- * @s: string.
- * @argv: user-given arguments.
- * @environ: environment variables.
- * @count: execution counter.
- * @file: file
- * @out: out
- * Return: out
- */
-int ex(char *s, char **argv, char *file, char **environ, int count, int out)
-{
-	(void)environ;
 
-	if (argv[1])
+/**
+ * add_key - create a new environment variable
+ * @vars: pointer to struct of variables
+ *
+ * Return: void
+ */
+void add_key(vars_t *vars)
+{
+	unsigned int i;
+	char **newenv;
+
+	for (i = 0; vars->env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * (i + 2));
+	if (newenv == NULL)
 	{
-		printerrorex(file, argv, count, "Illegal number");
-		return (127);
+		print_error(vars, NULL);
+		vars->status = 127;
+		new_exit(vars);
 	}
-	free(argv);
-	free(s);
-	exit(out);
+	for (i = 0; vars->env[i] != NULL; i++)
+		newenv[i] = vars->env[i];
+	newenv[i] = add_value(vars->av[1], vars->av[2]);
+	if (newenv[i] == NULL)
+	{
+		print_error(vars, NULL);
+		free(vars->buffer);
+		free(vars->commands);
+		free(vars->av);
+		free_env(vars->env);
+		free(newenv);
+		exit(127);
+	}
+	newenv[i + 1] = NULL;
+	free(vars->env);
+	vars->env = newenv;
 }
-/**
- * envir - print environment variables.
- * @s: string.
- * @argv: user-given arguments.
- * @environ: environment variables.
- * @count: execution counter.
- * @file: file
- * @out: out
- * Return: 0
- */
-int envir(char *s, char **argv, char *file, char **environ, int count, int out)
-{
-	int i = 0;
-	(void)s;
 
-	if (argv[1] != NULL)
+/**
+ * find_key - finds an environment variable
+ * @env: array of environment variables
+ * @key: environment variable to find
+ *
+ * Return: pointer to address of the environment variable
+ */
+char **find_key(char **env, char *key)
+{
+	unsigned int i, j, len;
+
+	len = _strlen(key);
+	for (i = 0; env[i] != NULL; i++)
 	{
-		printerror(file, argv[0], count, "No such file or directory");
-		return (127);
+		for (j = 0; j < len; j++)
+			if (key[j] != env[i][j])
+				break;
+		if (j == len && env[i][j] == '=')
+			return (&env[i]);
 	}
-	while (environ[i])
+	return (NULL);
+}
+
+/**
+ * add_value - create a new environment variable string
+ * @key: variable name
+ * @value: variable value
+ *
+ * Return: pointer to the new string;
+ */
+char *add_value(char *key, char *value)
+{
+	unsigned int len1, len2, i, j;
+	char *new;
+
+	len1 = _strlen(key);
+	len2 = _strlen(value);
+	new = malloc(sizeof(char) * (len1 + len2 + 2));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; key[i] != '\0'; i++)
+		new[i] = key[i];
+	new[i] = '=';
+	for (j = 0; value[j] != '\0'; j++)
+		new[i + 1 + j] = value[j];
+	new[i + 1 + j] = '\0';
+	return (new);
+}
+
+/**
+ * _atoi - converts a string into an integer
+ * @str: string to convert
+ *
+ * Return: the integer value, or -1 if an error occurs
+ */
+int _atoi(char *str)
+{
+	unsigned int i, digits;
+	int num = 0, num_test;
+
+	num_test = INT_MAX;
+	for (digits = 0; num_test != 0; digits++)
+		num_test /= 10;
+	for (i = 0; str[i] != '\0' && i < digits; i++)
 	{
-		_putchar(environ[i]);
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
+		num *= 10;
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
+			return (-1);
+		num += str[i] - '0';
+		if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
+			return (-1);
 	}
-	return (out);
+	if (i > digits)
+		return (-1);
+	return (num);
 }
