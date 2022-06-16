335071cdf06be856d072cc380160a9798634cf95
haywhiz <54290706+haywhiz@users.noreply.github.com>
2022-06-16 15:10:08 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -6,5 +6,9 @@ char **alloc_mem(char *input_command, unsigned int words);
 void freeCommand_arr(char **cmd);
 unsigned int num_of_words(char *input);
 void create_child(char **command_args);
+void sig_handler(int sig_num)
+void _EOF(int len, char *buff)
+void _isatty(void)
+
 
 #endif
